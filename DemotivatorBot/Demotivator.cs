e8a5db0788c271199189be91e57be114abed0e91
Egor Fomkin <efphooo@gmail.com>
2023-02-16 02:18:31 +0700

    Добавьте файлы проекта.

---
+++ DemotivatorBot/Demotivator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DemotivatorBot
+{
+    internal class Demotivator
+    {
+        private string demotivatorText = "";
+        private string fileToDemotivatePath = "";
+        private string resultPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\picsoutput\output.jpg";
+
+
+        public string DemotivatorText { get => DemotivatorText; set => DemotivatorText = value; }
+        public string FileToDemotivatePath { get => fileToDemotivatePath; set => fileToDemotivatePath = value; }
+        public string ResultPath { get => resultPath; set => resultPath = value; }
+
+        public Demotivator(string text, string file)
+        {
+            demotivatorText = text;
+            fileToDemotivatePath = file;
+        }
+
+        public void Demotivate() 
+        {
+            var background = Brushes.Black;
+            var textColor = Brushes.White;
+
+            var gap = 20;
+            var fontSize = 70;
+
+            var dpi = 96;
+
+            var font =
+                new Typeface(
+                    new FontFamily("Segoe UI"), FontStyles.Normal,
+                    FontWeights.Bold, FontStretches.SemiExpanded);
+            // <--
+
+            var image = BitmapFrame.Create(new Uri("file://" + fileToDemotivatePath));
+            var imageWidth = (double)image.PixelWidth;
+            var imageHeight = (double)image.PixelHeight;
+
+            var formattedText =
+            new FormattedText(
+                    demotivatorText, CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                    font, fontSize, textColor, dpi)
+                {
+                    MaxTextWidth = imageWidth,
+                    TextAlignment = TextAlignment.Center
+                };
+
+            var textWidth = formattedText.Width;
+            var textHeight = formattedText.Height;
+
+            var totalWidth = (int)Math.Ceiling(imageWidth + 2 * gap);
+            var totalHeight = (int)Math.Ceiling(imageHeight + 3 * gap + textHeight);
+
+            var drawingVisual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawRectangle(
+                    background, null,
+                    new Rect(0, 0, totalWidth, totalHeight));
+
+                drawingContext.DrawImage(
+                    image,
+                    new Rect(gap, gap, imageWidth, imageHeight));
+                drawingContext.DrawText(
+                    formattedText,
+                    new Point(gap, imageHeight + 2 * gap));
+            }
+
+            var bmp =
+                new RenderTargetBitmap(
+                    totalWidth, totalHeight, dpi, dpi,
+                    PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+
+            var encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+            using (var stream = System.IO.File.Create(ResultPath))
+                encoder.Save(stream);
+
+            return;
+        } 
+    }
+}
---
+++ DemotivatorBot/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using DemotivatorBot;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+
+var botClient = new TelegramBotClient("5897472120:AAGGvBDZki8avHeY9Nr1NiVsTzzrHkB_ENs");
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    HandleUpdateAsync,
+    HandlePollingErrorAsync,
+    receiverOptions,
+    cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+//Send cancellation request to stop
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    if (message.Text != null)
+    {
+        string messageText = message.Text;
+        Console.WriteLine($"{chatId}: {message.Chat.FirstName}   |Text: {messageText}");
+        return;
+    }
+
+    if (message.Photo != null)
+    {
+        await botClient.SendTextMessageAsync(
+            chatId,
+            "Пожалуйста, отправьте документом в формате JPG с описанием(оно будет подписью на вашем изображении).");
+        Console.WriteLine($"{chatId}: {message.Chat.FirstName}   |Photo");
+        return;
+    }
+
+    if (message.Document != null)
+    {
+        if (message.Caption == null) 
+        {
+            await botClient.SendTextMessageAsync(
+            chatId,
+            "Пожалуйста, отправьте документом в формате JPG с описанием(оно будет подписью на вашем изображении).");
+            return;
+        }
+
+        string caption = message.Caption;
+        Console.WriteLine($"{chatId}: {message.Chat.FirstName}   |Caption: {message.Caption}");
+
+        var fileId = message.Document.FileId;
+        var fileInfo = await botClient.GetFileAsync(fileId);
+        var filePath = fileInfo.FilePath;
+
+        if (filePath == null)
+        {
+            Console.WriteLine("filePath null");
+            return;
+        }
+
+        string destinationFilePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\picsinput\{message.Document.FileName}";
+
+        Demotivator d = new Demotivator(caption, destinationFilePath);
+
+        await using Stream fileStream = System.IO.File.OpenWrite(destinationFilePath);
+        await botClient.DownloadFileAsync(filePath, fileStream);
+        fileStream.Close();
+
+        d.Demotivate();
+
+        await using Stream stream = System.IO.File.OpenRead(d.ResultPath);
+        await botClient.SendDocumentAsync(chatId, new InputOnlineFile(stream, "pic.jpg"), "Вот ваша картинка.");
+
+        stream.Close();
+
+        return;
+    }
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
